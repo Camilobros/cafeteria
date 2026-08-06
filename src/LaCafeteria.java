a3554f9d8aafa7485f39e59e00eff829fe92937e
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-08-06 14:32:14 -0500

    agragar ejercicio 3

--- src/LaCafeteria.java
+++ src/LaCafeteria.java
@@ -34,5 +34,38 @@ public class LaCafeteria {
         System.out.printf("¿Tiene descuento?: %b%n", aplicaDescuento);
         System.out.printf("Valor del descuento: $%.2f%n", valorDescuento);
         System.out.printf("Total a pagar: $%.2f%n", totalAPagar);
+
+
+
+        char catebebidascalientes = 'B';
+        char catebebidasfries = 'F';
+        char catecomida = 'C';
+        char catepostre = 'P';
+
+        char categoriapedida = 'B';
+
+        switch (categoriapedida) {
+            case 'B':
+                System.out.println(" Su categoria es de bebidas calientes y tiene un iva del 0%");
+                break;
+
+            case 'F':
+                System.out.println("Su categoria es bebidas calientes y tiene un iva del 5%");
+                break;
+
+                case 'C':
+                    System.out.println("Su categoria es de comida y tiene un iva del 8%");
+                    break;
+
+            case 'P':
+                System.out.println("Su  categoria es postres y tiene un iva del 8%");
+
+                break;
+
+
+            default:
+                System.out.println("Su codigo no es una categoria valida");
+
+        }
     }
 }
